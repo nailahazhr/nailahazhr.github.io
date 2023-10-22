04f0684eb7dbc40de64f595dd872b4229282ba35
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2023-10-22 17:24:58 +0700

    010_Nailah.js

--- 010_Nailah.js
+++ 010_Nailah.js
@@ -1,52 +1,50 @@
-var statement = document.querySelector(".statement");
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
+    clearError();
+    let valid = true;
 
     // Ambil data dari formulir
+    var statement = document.querySelector(".statement");
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
 
-    clearErrorMessages();
-    let isValid = true;
-
     if (!name) {
-        isValid = false;
-        displayErrorMessage("name-error", "Nama wajib diisi.");
+        valid = false;
+        displayError("name-error", "Nama wajib diisi.");
     }
 
     if (!email) {
-        isValid = false;
-        displayErrorMessage("email-error", "Email wajib diisi.");
+        valid = false;
+        displayError("email-error", "Email wajib diisi.");
     }
 
     if (!subject) {
-        isValid = false;
-        displayErrorMessage("subject-error", "Email Subject wajib diisi.");
+        valid = false;
+        displayError("subject-error", "Subject wajib diisi.");
     }
 
     if (!message) {
-        isValid = false;
-        displayErrorMessage("message-error", "Pesan wajib diisi.");
+        valid = false;
+        displayError("message-error", "Pesan wajib diisi.");
     }
 
-    if (!isValid) {
-        event.preventDefault(); // Mencegah pengiriman formulir jika tidak valid
+    if (!valid) {
+        event.preventDefault();
     } else {
         statement.innerHTML = "Pesan telah terkirim.";
     }
 
-    function displayErrorMessage(id, message) {
-        const errorSpan = document.getElementById(id);
-        errorSpan.innerHTML = message;
-        errorSpan.style.color = "red";
+    function displayError(id, message) {
+        const errorMessage = document.getElementById(id);
+        errorMessage.innerHTML = message;
     }
 
-    function clearErrorMessages() {
-        const errorSpans = document.querySelectorAll(".error");
-        errorSpans.forEach(function (errorSpan) {
-            errorSpan.innerHTML = "";
+    function clearError() {
+        const errors = document.querySelectorAll(".error");
+        errors.forEach(function (errorMessage) {
+            errorMessage.innerHTML = "";
         });
     }
 });
